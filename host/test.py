af39d4dd98ec5e7e76955abe12b02fd97ee332cc
zhoupeng <zp@localhost.localdomain>
2011-05-03 17:54:37 +0800

    host:test.py, test program.

---
+++ host/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Filename: host/smMain.py
+#
+# -------------------------------------------------------------------
+#
+# 
+#
+# -------------------------------------------------------------------
+
+import smXen 
+import sys
+import simplejson as json
+class Test: 
+    def _GetOutput(self):
+        """Returns the combined stdout and stderr for easier usage.
+    
+        """
+        #return sys.stdout + sys.stderr
+        return "property test.", self.output 
+
+    output = property(_GetOutput, None, None, "Return full output")
+
+
+if __name__ == '__main__':
+
+    #test = Test()
+    #print (test._GetOutput())
+    xennode = smXen.XenNode()
+
+    print smXen.XenNode._getXMList(include_node = True)
+    print xennode.getNodeInfo();
+    print xennode.verify()
+
