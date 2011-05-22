2ade1b4fbf3f6a6cb63bb48c4744ad605f21ffe1
zhoupeng <zp@localhost.localdomain>
2011-05-22 17:52:25 +0800

    host:smHost, (1) Add AgentCMDThread to receive cmd from the agent; (2) Add Host.running to tag host's state; (3) Fix report field naming err and some syntax err in timer function Host._resourceReport

--- host/smHost.py
+++ host/smHost.py
@@ -46,6 +46,9 @@ class Host(object):
         self.__uuid = smIO.NewUUID()
         # periodical resource reporter
         self.rsReporter = None
+        # thread to response to command from agent.
+        self.agentCMDThread = None
+        self.running = False
 
     def getType(self):
         """ get the type of this host node
@@ -89,23 +92,52 @@ class Host(object):
         pass
 
     def start(self):
+        self.running = True
         # periodical resource reporter
         self.rsReporter = smTimer.BackgroundTask(RS_REPORT_INTERVAL, 
                                                  Host._resourceReport, [self])
+        self.agentCMDThread = AgentCMDThread(self)
+        self.agentCMDThread.start()
         self.rsReporter.start()
+        self.agentCMDThread.join()
 
     @staticmethod
-    def _resourceReport(thisHost):
+    def _resourceReport(host):
         """ The timer function of rsReporter.
         """
-        cpuuse = thisHost.node.getCPUUsage()
-        nodeinfo = thisHost.node.getNodeInfo()
+
+        if not host.running:
+            host.rsReporter.cancel()
+            return
+
+        cpuuse = host.node.getCPUUsage()
+        nodeinfo = host.node.getNodeInfo()
 
         report = {}
         report.update(cpurate = cpuuse['cpurate'],
-                      total_memory = nodeinfo['total_memory'],
-                      free_memory = nodeinfo['free_memory'],
+                      memory_total = nodeinfo['memory_total'],
+                      memory_free = nodeinfo['memory_free'],
                       memory_dom0 = nodeinfo['memory_dom0'])
         # send resource report, ** used for keyword args(that is dict)
-        self.sock.send(CMDHostAgent.cmd_rsreport(self.getUUID(), **report))
+        host.sock.send(CMDHostAgent.cmd_rsreport(host.getUUID(), **report))
+
+
+class AgentCMDThread(threading.Thread):
+    """ thread to response to command from agent.
+    """
+
+    def __init__(self, host):
+        """
+        @type host: Host
+        @parem host: the served host
+        """
+        threading.Thread.__init__(self)
+
+        self.host = host
+
+    def run(self):
+        while self.host.running:
+            cmd = self.host.sock.recv(512)
+            if cmd:
+                print cmd
 
