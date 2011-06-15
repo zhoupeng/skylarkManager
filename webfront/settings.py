44d0779582c9af73e4c98b5b46d863003fc82e34
zhoupeng <zp@localhost.localdomain>
2011-06-15 10:59:51 +0800

    webfront: use relative path for TEMPLATE_DIRS. django setting.py's comment says to use absolute path seem not be proper.
    modified:   webfront/settings.py

--- webfront/settings.py
+++ webfront/settings.py
@@ -106,7 +106,9 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/zp/skylarkManager/webfront/templates"
+
+    # It works for relative path
+    "./templates"
 )
 
 INSTALLED_APPS = (
