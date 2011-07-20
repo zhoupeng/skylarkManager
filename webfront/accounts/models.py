eae0fae76793b26c2908bccf4bc01d152ee65463
zhoupeng <zp@localhost.localdomain>
2011-07-20 10:58:04 +0800

    webfront: add models
    modified:   webfront/accounts/models.py
    modified:   webfront/settings.py

--- webfront/accounts/models.py
+++ webfront/accounts/models.py
@@ -15,3 +15,61 @@ There are 3 models in django.contrib.auth.models:
  * Groups
 This module will defined necessary models addtionally.
 """
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Service(models.Model):
+    """The service provided, including vApp, vDesktop, server, fileman.
+    The different params will be patched as different service
+
+    @filed type: the service type or the name of the service
+    @filed logo: the file name of logo picture (file path not included)
+    @field description: introduce to this service
+    @field price: The price of this service, reserved
+
+    params below reserved for vDesktop or server
+    @field disksize: reserved
+    @field memsize: reserved
+    @field netspeed: reserved
+    @field cpunum: reserved
+    """
+
+    type = models.CharField(max_length = 50, primary_key = True)
+    logo = models.CharField(max_length = 50, null = True)
+    description = models.CharField(max_length = 500, null = True)
+
+class OrderState:
+    """
+    0-ordered, this service is in initial state, no resource allocated
+    1-running, this service is running
+    2-saved, this service is saved in snapshot and stoped
+    3-stoped, this service stops running without snapshot,
+              but the image kept
+    4-canceled, this order is canceled, the image and snapshot are released
+    """
+    ORDERED = 0
+    RUNNING = 1
+    SAVED = 2
+    STOPED = 3
+    CANCELED = 4
+
+class Order(models.Model):
+    """User order
+    @filed user: the user of this order
+    @field sercive: the service of this order
+    @field num: number of user's some service order,
+                local to each user's each service type, begin with 0
+    @field state:
+        0-ordered, this service is in initial state, no resource allocated
+        1-running, this service is running
+        2-saved, this service is saved in snapshot and stoped
+        3-stoped, this service stops running without snapshot,
+                  but the image kept
+        4-canceled, this order is canceled, the image and snapshot are released
+    """
+
+    user = models.ForeignKey(User)
+    service = models.ForeignKey(Service)
+    num = models.IntegerField()
+    state = models.IntegerField()
