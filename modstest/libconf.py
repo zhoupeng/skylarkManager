5cf3aac0fba8033136d47238570333a8f0ee565b
zhoupeng <zp@localhost.localdomain>
2011-10-28 19:00:49 +0800

    modstest: add test dir to hold scripts for module testing
    new file:   modstest/README
    new file:   modstest/libconf.py

---
+++ modstest/libconf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# Filename: modstest/libconf.py
+#
+# -------------------------------------------------------------------
+#
+# Author:
+#  Zhou Peng <ailvpeng25@gmail.com>, 2011.10 ~
+#
+# -------------------------------------------------------------------
+"""
+Adding lib and common to the python path
+"""
+import sys
+import os
+lib_path = os.path.abspath('../lib')
+common_path = os.path.abspath('../common')
+sys.path.append(lib_path)
+sys.path.append(common_path)
