94d0e4361aee408ca09193c9284af9b3aabafe66
Zhou Peng <ailvpeng25@gmail.com>
2012-03-20 16:02:22 +0800

    agent: use smAgentMain.py to replace test.py
    modified:   agent/smAgentMain.py

--- agent/smAgentMain.py
+++ agent/smAgentMain.py
@@ -5,8 +5,25 @@
 # -------------------------------------------------------------------
 #
 # Author:
-#  Zhou Peng <ailvpeng25@gmail.com>, 2011.04 ~
+#  Zhou Peng <ailvpeng25@gmail.com>, 2012.03 ~
 #
 # -------------------------------------------------------------------
+import libconf
+from CONSTANTS import *
+import smListeners
+import smClient
+from smSchedule import Scheduler
 
+if __name__ == '__main__':
+    hostListener = smListeners.Listener(port = MASTER_AGENT_PORT,
+                                        type = "host")
+    hostListener.start()
 
+    clientSrv = smClient.Client(port = CLIENTSRV_PORT,
+                                host = CLIENTSRV_HOST)
+    clientSrv.start()
+
+    sched = Scheduler.getInstance()
+    sched.setMethod(Scheduler.M_CYCLIC)
+    hostListener.join()
+    clientSrv.join()
